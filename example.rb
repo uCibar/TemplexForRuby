9a941327a611c4b64223c73d2d68d00426a370be
Uğur Cibaroğlu <ugur.cibar@gmail.com>
2016-03-10 01:56:35 +0200

    line method added

--- example.rb
+++ example.rb
@@ -2,4 +2,5 @@ require "./templex"
 
 view = Templex.new
 
-view.window(width: 80, height:25, format:[1,100])
+view.window(width: 80, height:30, format:[1,100])
+view.line(text:"#-#", margin:10, format:[31])
