646c328bebbbedadcf8cf5c974b3974805887aae
Uğur Cibaroğlu <ugur.cibar@gmail.com>
2016-03-10 04:16:06 +0200

    Update example.rb

--- example.rb
+++ example.rb
@@ -3,8 +3,13 @@ require "./templex"
 view = Templex.new
 
 view.window(width: 80, height:30, format:[1,100])
+
 view.line(text:"#-#", margin:10, format:[31])
+view.line(text:"_", margin:20)
 view.newLine
+
 view.title(text:"My Awesome Terminal Application", center:true, format:[32])
+
+view.line(text:"_", margin:20)
 view.newLine
 view.line(text:"#-#", margin:10, format:[31])
