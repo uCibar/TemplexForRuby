ea5bc4f663884b8df1d785b0eb38b583b82d3a8a
Uğur Cibaroğlu <ugur.cibar@gmail.com>
2016-03-10 03:55:09 +0200

    title method added

--- example.rb
+++ example.rb
@@ -4,3 +4,7 @@ view = Templex.new
 
 view.window(width: 80, height:30, format:[1,100])
 view.line(text:"#-#", margin:10, format:[31])
+view.newLine
+view.title(text:"My Awesome Terminal Application", center:true, format:[32])
+view.newLine
+view.line(text:"#-#", margin:10, format:[31])
